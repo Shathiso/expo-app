11230cd27d2e85f73aa55efb76ec305c704275aa
Shathiso <shathisontibi@yahoo.com>
2024-06-23 10:37:10 +0200

    completed the demo info page

--- app/(tabs)/information.tsx
+++ app/(tabs)/information.tsx
@@ -1,95 +1,69 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform } from 'react-native';
+import { StyleSheet, Image, Platform, SafeAreaView, View, Text, ScrollView} from 'react-native';
 
 import { Collapsible } from '@/components/Collapsible';
 import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import LogoHeader from '@/components/LogoHeader';
+import ParallaxScrollView from '../../components/ParallaxScrollView'
 
 export default function Information() {
 
   return (
-    <ParallaxScrollView
+    <>
+      <LogoHeader />
+      
+      <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Information</ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText> library
-          to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+      headerImage={<Image
+        style={styles.mainImage}
+        source={require('@/assets/images/slideshow/slide-2-resized.png')}
+        resizeMode='cover'
+      />}>
+        <View >
+          <Text style={styles.pageTitle}>Information</Text>
+          <Text style={styles.subTitle}>BHC Knowledge base</Text>
+          <View style={styles.contentContainer}>
+            <Collapsible title="What is a sale agreement">
+              <Text>
+                  It is a contract between the purchaser and a seller which sets out the terms and conditions of a sale.
+              </Text>
+              <ExternalLink href="https://bhc.bw/faqs?page=1">
+                <Text>Learn more</Text>
+              </ExternalLink>
+            </Collapsible>
+            <Collapsible title="When will BHC start selling flats and townhouses?">
+              <Text>
+                BHC has started selling flats and town houses through Sectional Titles. The implementation started with the 64 flats at Block 9, Gaborone. However, the selling of same countrywide shall be done on a "roll out" basis.
+              </Text>
+            </Collapsible>
+            <Collapsible title="What is your Customer Call Centre Phone Number? Is there a toll free number? ">
+              <Text>
+                  Call (+267)31559902 or 1167, there is no toll free number. 
+              </Text>
+            </Collapsible>
+          </View>
+        </View>
+      </ParallaxScrollView>
+      </>
   );
 }
 
 const styles = StyleSheet.create({
+  safeAreaView:{
+    height: '100%'
+  },
+  pageTitle:{
+    fontFamily:'Poppins-SemiBold',
+    fontSize:24,
+    textAlign:'center',
+    marginTop:20
+  },
+  subTitle:{
+    fontFamily:'Poppins-Regular',
+    fontSize:16,
+    textAlign:'center',
+    marginTop:10
+  },
   headerImage: {
     color: '#808080',
     bottom: -90,
@@ -99,5 +73,46 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
     gap: 8,
+  },
+  container:{
+    flex:1
+  },
+  logoContainer:{
+    backgroundColor:'white',
+    width: 400,
+    maxHeight:60,
+    height: 60,
+    flex:1
+  },
+  logo:{
+    width:100,
+    height:60
+  },
+  mainImageContainer:{
+    flex:1,
+    maxHeight:185,
+  },
+
+  mainImage:{
+    position:'absolute',
+    left:0,
+    top:0,
+    height: 185,
+    width:400
+  },
+  contentContainer:{
+    flex: 1,
+    padding: 18,
+    gap: 16,
+    overflow:"scroll",
+    minHeight: "87%"
+  },
+  title:{
+    fontSize:18
+  },
+  text: {
+    fontSize: 28,
+    lineHeight: 32,
+    marginTop: -6,
   },
 });
